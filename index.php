a84c36ff97094006f70a36bc93c1901878053312
okabayashis <okabayashis8621@sbtjapan.com>
2024-11-11 14:49:29 +0900

    output many case of geting ip

--- index.php
+++ index.php
@@ -28,6 +28,17 @@
             $which_ip = "loopback0";
         }
         echo $which_ip;
+        echo "=========<br>"
+        $output = <<<EOL
+        <pre>
+        REMOTE_ADDR              : {$_SERVER['REMOTE_ADDR']}
+        HTTP_X_FORWARDED_FOR     : {$_SERVER['HTTP_X_FORWARDED_FOR']}
+        HTTP_CLIENT_IP           : {$_SERVER['HTTP_CLIENT_IP']}
+        HTTP_CLIENTADDRESS       : {$_SERVER['HTTP_CLIENTADDRESS']}
+        HTTP_X_REAL_IP           : {$_SERVER['HTTP_X_REAL_IP']}
+        HTTP_X_REAL_FORWARDED_FOR: {$_SERVER['HTTP_X_REAL_FORWARDED_FOR']}
+EOL;
+        echo "=========<br>"
         $url = "https://api.iplocation.net/?ip=".$ip;
         $json = mb_convert_encoding(file_get_contents($url), 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
         $json_arr = json_decode($json,true);
