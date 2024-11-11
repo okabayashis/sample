b7607cfbd79d6791b17188b0fc7bf947e1df39ab
okabayashis <okabayashis8621@sbtjapan.com>
2024-11-11 12:11:16 +0900

    to fix, get ip

--- index.php
+++ index.php
@@ -6,7 +6,19 @@
     <body>
         <?php
         echo '<p>Hello World</p>';
-        $ip = $_SERVER['X-Forwarded-For'];
+        $ip = "127.0.0.1";
+        if (isset($_SERVER['http_cf_connecting_ip'])) { // Cloudflare対応
+            $ip = $_SERVER['http_cf_connecting_ip'];
+        } elseif (isset($_SERVER['REMOTE_ADDR']) === true) {
+            $ip = $_SERVER['REMOTE_ADDR'];
+            if (preg_match('/^(?:127|10)\.0\.0\.[12]?\d{1,2}$/', $ip)) {
+                if (isset($_SERVER['HTTP_X_REAL_IP'])) {
+                    $ip = $_SERVER['HTTP_X_REAL_IP'];
+                } elseif (isset($_SERVER['http_x_forwarded_for'])) {
+                    $ip = $_SERVER['http_x_forwarded_for'];
+                }
+            }
+        } 
         $url = "https://api.iplocation.net/?ip=".$ip;
         $json = mb_convert_encoding(file_get_contents($url), 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
         $json_arr = json_decode($json,true);
@@ -17,7 +29,7 @@
             $country_name = $json_arr["country_name"];
             $country_code = $json_arr["country_code2"];
             echo "<p>your country is $country_name($country_code)</p>";
-            var_dump($json_arr);
+            //var_dump($json_arr);
         }
         ?>
     </body>
