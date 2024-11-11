c7d08212d546c2e7aaf29a314c233ba1943f0128
okabayashis <okabayashis8621@sbtjapan.com>
2024-11-11 14:23:17 +0900

    dbg msg added

--- index.php
+++ index.php
@@ -7,19 +7,25 @@
         <?php
         echo '<p>Hello World</p>';
         $ip = "";
+        $which_ip = ""
         if (isset($_SERVER['http_cf_connecting_ip'])) { // Cloudflare対応
             $ip = $_SERVER['http_cf_connecting_ip'];
+            $which_ip = "http_cf_connectiong_ip";
         } elseif (isset($_SERVER['REMOTE_ADDR']) === true) {
             $ip = $_SERVER['REMOTE_ADDR'];
+            $which_ip = "REMOTE_ADDR";
             if (preg_match('/^(?:127|10)\.0\.0\.[12]?\d{1,2}$/', $ip)) {
                 if (isset($_SERVER['HTTP_X_REAL_IP'])) {
                     $ip = $_SERVER['HTTP_X_REAL_IP'];
+                    $which_ip = "HTTP_X_REAL_IP";
                 } elseif (isset($_SERVER['http_x_forwarded_for'])) {
                     $ip = $_SERVER['http_x_forwarded_for'];
+                    $which_ip = "http_x_forwarded_for";
                 }
             }
         } else {
             $ip = "127.0.0.1";
+            $which_ip = "loopback0";
         }
         $url = "https://api.iplocation.net/?ip=".$ip;
         $json = mb_convert_encoding(file_get_contents($url), 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
